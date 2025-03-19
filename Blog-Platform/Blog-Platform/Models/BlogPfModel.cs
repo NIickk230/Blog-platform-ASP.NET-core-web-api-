930cd85a8ffacbb7813650da77abdcdec24af7cb
NIickk230 <n.megr23@gmail.com>
2025-03-19 23:46:42 +0100

    1

--- Blog-Platform/Blog-Platform/Models/BlogPfModel.cs
+++ Blog-Platform/Blog-Platform/Models/BlogPfModel.cs
@@ -2,5 +2,16 @@
 {
     public class BlogPfModel
     {
+        private string Author { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+
+        public BlogPfModel(string author, string title, string description)
+        {
+            Author = author;
+            Title = title;
+            Description = description;
+        }
+
     }
 }
